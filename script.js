24985519a44f814dd35be11e6166b478ad0e40b5
Temurkan <87571221+Temurkan@users.noreply.github.com>
2025-07-26 14:26:55 +0500

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+///// 1 /////
+
+// function checkIfEven() {
+//   const input = prompt("Введите число:");
+//   const number = Number(input);
+
+//   if (isNaN(number)) {
+//     alert("Пожалуйста, введите корректное число.");
+//     return;
+//   }
+
+//   if (number % 2 === 0) {
+//     alert("Число делится на 2.");
+//   } else {
+//     alert("Число не делится на 2.");
+//   }
+// }
+
+// checkIfEven();
